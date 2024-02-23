af1e27cd1d5f5af5bac392c9efd4a209200ec9fa
Ricky Anchores <66031906+rickyanchores@users.noreply.github.com>
2024-02-23 12:55:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='App flex items-center justify-center min-h-screen'>
+      <div className="bg-red-500 p-4">
+        <h1 className='title text-4xl font-bold font-mono'>Poke-dex</h1>
+        <p>Poke API will be here soon</p>
+      </div>
+    </div>
+  )
+}
+
+export default App;
