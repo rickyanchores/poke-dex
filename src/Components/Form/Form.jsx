4a5881c12f671044478f87924e0c7d83c81f5f84
RickyAnchores <ricky23anchores@gmail.com>
2024-03-10 17:19:03 +0100

    update

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -8,7 +8,7 @@ const Form = () => {
   return (
     <div className='Form bg-slate-900 p-4 flex justify-center items-center md:col-span-3 md:row-span-2'>
         <div className="container">
-            <input type="text" placeholder='Search Pokemon'/>
+            <input className='' type="text" placeholder='Search Pokemon'/>
             <button className="btn hover:animate-pulse" onClick={() => console.log("Hello")}>Find Poke</button>
         </div>
     </div>
